204ca93d3d6ce91e39609ec513942fa2c001d609
Tullio Sebastiani <tsebasti@redhat.com>
2024-10-25 16:37:42 +0200

    added podman to github action
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    added socket
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    fix
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    podman socket
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    test command
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    gotestfmt
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    gotestfmt
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    kind
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    fixed test
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    kubeconfig
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
     container socket
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    socket
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    test
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    print err
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    error
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    fix
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    err
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    user
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    user
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    sudo
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    test
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    env
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    uid
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    uid
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    removed sudo
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    uid
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    uid
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    atoi
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    pattern
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    ubuntu 24.04
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    24.04
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    start podman
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- pkg/container_manager/podman/container_manager_test.go
+++ pkg/container_manager/podman/container_manager_test.go
@@ -1,10 +1,14 @@
 package podman
 
 import (
+	"fmt"
 	krknctlconfig "github.com/krkn-chaos/krknctl/internal/config"
 	"github.com/krkn-chaos/krknctl/pkg/container_manager"
 	"github.com/krkn-chaos/krknctl/pkg/provider/quay"
 	"github.com/stretchr/testify/assert"
+	"os"
+	"os/user"
+	"strconv"
 	"testing"
 )
 
@@ -13,13 +17,13 @@ func getTestConfig() krknctlconfig.Config {
 		Version:              "0.0.1",
 		QuayProtocol:         "https",
 		QuayHost:             "quay.io",
-		QuayOrg:              "redhat-chaos",
+		QuayOrg:              "krkn-chaos",
 		QuayRegistry:         "krkn-hub",
 		QuayRepositoryApi:    "api/v1/repository",
 		ContainerPrefix:      "krknctl-containers",
 		KubeconfigPrefix:     "krknctl-kubeconfig",
 		DarwinSocketTemplate: "unix://%s/.local/share/containers/podman/machine/podman.sock",
-		LinuxSocketTemplate:  "unix://run/user/%s/podman/podman.sock",
+		LinuxSocketTemplate:  "unix://run/user/%d/podman/podman.sock",
 		LinuxSocketRoot:      "unix://run/podman/podman.sock",
 	}
 }
@@ -35,17 +39,38 @@ func TestConnect(t *testing.T) {
 	cm := ContainerManager{
 		Config: conf,
 	}
+	currentUser, err := user.Current()
+	fmt.Println("Current user: " + (*currentUser).Name)
+	fmt.Println("current user id" + (*currentUser).Uid)
 	quayProvider := quay.ScenarioProvider{}
-	scenario, err := quayProvider.GetScenarioDetail("cpu-hog", conf.GetQuayRepositoryApiUri())
+	scenario, err := quayProvider.GetScenarioDetail("node-cpu-hog", conf.GetQuayRepositoryApiUri())
 	assert.Nil(t, err)
+	assert.NotNil(t, scenario)
 	kubeconfig, err := container_manager.PrepareKubeconfig(nil, getTestConfig())
 	assert.Nil(t, err)
-	socket, err := cm.GetContainerRuntimeSocket()
+	assert.NotNil(t, kubeconfig)
+	fmt.Println("KUBECONFIG PARSED -> " + *kubeconfig)
+
+	envuid := os.Getenv("USERID")
+	var uid *int = nil
+	if envuid != "" {
+		_uid, err := strconv.Atoi(envuid)
+		assert.Nil(t, err)
+		uid = &_uid
+		fmt.Println("USERID -> ", *uid)
+	}
+	socket, err := cm.GetContainerRuntimeSocket(uid)
 	assert.Nil(t, err)
+	assert.NotNil(t, socket)
+
+	fmt.Println("CONTAINER SOCKET -> " + *socket)
 	containerId, err := cm.RunAndStream(conf.GetQuayImageUri()+":"+scenario.Name,
 		scenario.Name,
 		*socket,
 		env, false, map[string]string{}, *kubeconfig, scenario.KubeconfigPath)
+	if err != nil {
+		fmt.Println("ERROR -> " + err.Error())
+	}
 	assert.Nil(t, err)
 	assert.NotNil(t, containerId)
 
