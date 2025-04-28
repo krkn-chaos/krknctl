465ec789e3e83db5235ef6fb6898842e5e5a8499
Paige Patton <prubenda@redhat.com>
2025-04-28 12:55:07 -0400

    updating test
    Signed-off-by: Paige Patton <prubenda@redhat.com>

--- pkg/typing/field_test.go
+++ pkg/typing/field_test.go
@@ -547,8 +547,6 @@ func TestFieldFile(t *testing.T) {
 	_, err = field.Validate(&fileNameNotAccessible)
 	assert.NotNil(t, err)
 
-	field = InputField{}
-
 	fileFieldNoMountPath := `
 {
 	"name":"cores",
