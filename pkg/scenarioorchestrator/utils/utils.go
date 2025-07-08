a3c341b8576ca6c5aa041558ec19ff0ab42a57e0
Tullio Sebastiani <tsebastiani@users.noreply.github.com>
2025-07-08 12:12:11 +0200

    changed flattened kubeconfig bitmask to 0644 (#72)
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- pkg/scenarioorchestrator/utils/utils.go
+++ pkg/scenarioorchestrator/utils/utils.go
@@ -105,7 +105,7 @@ func PrepareKubeconfig(kubeconfigPath *string, config config.Config) (*string, e
 	}
 	filename := fmt.Sprintf("%s-%s-%d", config.KubeconfigPrefix, text.RandString(5), time.Now().Unix())
 	path := filepath.Join(currentDirectory, filename)
-	err = os.WriteFile(path, flattenedConfig, 0600)
+	err = os.WriteFile(path, flattenedConfig, 0644) /* #nosec */
 	if err != nil {
 		return nil, err
 	}
