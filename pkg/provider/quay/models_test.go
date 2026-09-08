d2f78b2d74385a34972cfd87e50333f00e7466bc
Tullio Sebastiani <tsebasti@redhat.com>
2026-09-08 16:00:05 +0200

    test(quay): cover image size resolution
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

---
+++ pkg/provider/quay/models_test.go
@@ -0,0 +1,60 @@
+package quay
+
+import (
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestManifestList_GetKrknctlManifest_MatchesArchitecture(t *testing.T) {
+	list := ManifestList{Manifests: []ManifestEntry{
+		{Digest: "sha256:other", Platform: Platform{Architecture: "other-arch"}},
+		{Digest: "sha256:matching", Platform: Platform{Architecture: runtime.GOARCH}},
+	}}
+
+	selected := list.GetKrknctlManifest()
+	require.NotNil(t, selected)
+	assert.Equal(t, "sha256:matching", selected.Digest)
+}
+
+func TestManifestList_GetKrknctlManifest_FallsBackToFirstUsable(t *testing.T) {
+	list := ManifestList{Manifests: []ManifestEntry{
+		{Digest: "", Platform: Platform{Architecture: runtime.GOARCH}},
+		{Digest: "sha256:first-usable", Platform: Platform{Architecture: "other-arch"}},
+		{Digest: "sha256:second-usable", Platform: Platform{Architecture: "another-arch"}},
+	}}
+
+	selected := list.GetKrknctlManifest()
+	require.NotNil(t, selected)
+	assert.Equal(t, "sha256:first-usable", selected.Digest)
+}
+
+func TestManifestList_GetKrknctlManifest_Empty(t *testing.T) {
+	assert.Nil(t, (ManifestList{}).GetKrknctlManifest())
+}
+
+func TestManifestImageSize_UsesAggregateSize(t *testing.T) {
+	size := manifestImageSize(Manifest{LayerCompressedSize: "5242880"})
+	require.NotNil(t, size)
+	assert.Equal(t, int64(5242880), *size)
+}
+
+func TestManifestImageSize_SumsLayersWhenAggregateMissing(t *testing.T) {
+	size := manifestImageSize(Manifest{Layers: []Layer{
+		{CompressedSize: 100},
+		{CompressedSize: 250},
+	}})
+	require.NotNil(t, size)
+	assert.Equal(t, int64(350), *size)
+}
+
+func TestManifestImageSize_UnknownForIncompleteLayers(t *testing.T) {
+	assert.Nil(t, manifestImageSize(Manifest{Layers: []Layer{
+		{CompressedSize: 100},
+		{CompressedSize: 0},
+	}}))
+	assert.Nil(t, manifestImageSize(Manifest{LayerCompressedSize: "not-a-number"}))
+	assert.Nil(t, manifestImageSize(Manifest{}))
+}
